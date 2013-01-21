1304ac97b2b0acac33dab3dcaff7910fa15a5840
HungryCoder <rajuru@gmail.com>
2013-01-21 23:36:36 +0600

    dynamic color changer in tabbed controller

--- app/controllers/contactdetail_controller.rb
+++ app/controllers/contactdetail_controller.rb
@@ -9,6 +9,22 @@ class ContactDetailController < UIViewController
     super
     self.view.backgroundColor = @color
     self.title = "Detail"
+    rightButton = UIBarButtonItem.alloc.initWithTitle("Change",
+          style: UIBarButtonItemStyleBordered,
+          target: self,
+          action: 'change_color'
+        )
+    self.navigationItem.rightBarButtonItem = rightButton
+  end
+
+  def change_color
+    controller = ChangeColorController.alloc.initWithNibName(nil, bundle:nil)
+    controller.detail_controller = self
+    self.presentViewController(
+      UINavigationController.alloc.initWithRootViewController(controller),
+      animated: true,
+      completion: lambda{}
+    )
   end
 end
 
